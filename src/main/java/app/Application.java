ffba93e25df4691bbb1d2f96c1a60e242ef0e63e
Maksim Petrov <dragon-black@mail.ru>
2021-01-22 00:23:21 +0300

    Задача 2.1.3

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.AppConfig;
 import app.model.AnimalsCage;
+import app.model.Timer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,9 +11,11 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         for (int i = 0; i < 5; i++) {
             AnimalsCage bean =
                     applicationContext.getBean(AnimalsCage.class);
+            System.out.println(bean.getTimer().getTime());
             bean.whatAnimalSay();
         }
     }
